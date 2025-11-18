530ec18469dfc0fbc665c3f13cfec3f514416908
Tushar Ahmad <146320779+MrTushar786@users.noreply.github.com>
2025-11-18 11:00:26 +0530

    Remove SpeedInsights from AppRouter
    Removed SpeedInsights component from AppRouter.

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -11,11 +11,9 @@ import MaintenanceRequest from "./component/MaintenanceRequest";
 import MessMenu from "./component/MessMenu";
 import Profile from "./component/Profile";
 import ComingSoon from "./component/ComingSoon";
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function AppRouter() {
   return (
-    <SpeedInsights />
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
